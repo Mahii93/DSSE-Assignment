acccdd72e3345e86e216218aec4578908c977643
Mahii93 <50650918+Mahii93@users.noreply.github.com>
2024-01-14 19:40:01 +0530

    Update bash.sh

--- Bash-cli/bash.sh
+++ Bash-cli/bash.sh
@@ -4,4 +4,5 @@ echo "This script is used for lookup service for pincode.
 read -p  "please enter the pincode: " pincode
 echo The pin code passed : $pincode
 cd /home/pin-code-CLI/
-curl -O data.json "https://www.travel-advisory.info/api -k"
+#copying the remote content to a file
+curl -v -u username:password -O data.json "https://www.travel-advisory.info/api -k "
