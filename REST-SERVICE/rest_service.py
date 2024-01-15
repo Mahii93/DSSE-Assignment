f8b7a5554c49e595dfedb7cf9abe3c5361a1f927
Mahii93 <50650918+Mahii93@users.noreply.github.com>
2024-01-15 15:25:44 +0530

    Update rest_service.py

--- REST-SERVICE/rest_service.py
+++ REST-SERVICE/rest_service.py
@@ -3,7 +3,15 @@ app= FastAPI()
 
 @app.get("/health")
 def health ():
-kubectl get pods 
+kubectl get pods > healthcheck.log
+kubetctl get nodes >> healthcheck.log
+grep "Not Ready" healthcheck.log
+if [ $? -eq 0 ] ;
+  then 
+    print "NODES AND PODS ARE NOT RUNNING FINE. Please check"
+else 
+    print "NODES AND PODS ARE  RUNNING FINE"
+fi;
 
 @app.get("/diag")  
 def status()
